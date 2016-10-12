62619876431572138e31b26ac13496e61ca50ea0
louisGan <louisGan1988@gmail.com>
2016-10-12 17:06:54 +0800

    多文件上传node实现

---
+++ examples/express4/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+//var webUploader = require('node-webuploader-server');//require('../../index');
+var webUploader = require('../../index');
+
+var path = require('path');
+
+var app = express();
+
+var router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express["static"](path.join(__dirname, 'public')));
+
+var uploaderConfig = {uploadDir:path.join(__dirname, 'public','uploads'),limitExtension:null};
+
+var uploder = new webUploader(uploaderConfig);
+uploder.mount(router);
+app.use('/upload',loginChecker,router);
+
+function loginChecker(req, res, next) {
+    //check user login
+    next();
+}
+
+app.get('/', function (req, res) {
+    res.redirect('/index.html');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
